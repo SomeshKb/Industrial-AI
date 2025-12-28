611b9cbcb09871101c98367cd641c495484d8cf9
Somesh K Bhaskar <someshkumarbhaskar@gmail.com>
2025-12-29 02:38:46 +0530

    added by id

--- routes/model.py
+++ routes/model.py
@@ -1,21 +1,19 @@
 from flask import Blueprint, request, jsonify
-from db import db
 from models import Anomaly_Model
+from services import anomaly_model_service
 model_bp = Blueprint("/", __name__)
 
 
 @model_bp.route("/", methods=["GET"])
 def hello():
-    items = Anomaly_Model.query.all()
-    return jsonify([item.to_dict() for item in items]), 200
+    return anomaly_model_service.get_all()
 
 @model_bp.route("/", methods=["POST"])
 def create_item():
-    data = request.get_json()
-    name = data["name"]
+        data = request.get_json()
+        name = data["name"]
+        return anomaly_model_service.create_anomaly_model(name)
 
-    anomaly_Model = Anomaly_Model(name=name)
-    db.session.add(anomaly_Model)
-    db.session.commit()
-
-    return jsonify(anomaly_Model.to_dict()), 201
+@model_bp.route("/<string:model_id>", methods=["GET"])
+def get_model_by_id(model_id):
+      return anomaly_model_service.get_by_id(model_id)
